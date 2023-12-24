86ef9df466a5c2a5ab46829bb8ea42cba24c164c
abhishek bambal <bambalabhishek026@gmail.com>
2023-12-24 16:10:07 +0530

    Initial commit

---
+++ StudentApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from StudentApp import views
+
+urlpatterns = [
+    path('',views.loginfun,name='login'),
+    path('register',views.registerfun,name='register'),
+    path('logout',views.logoutfun,name='logout'),
+    path('home',views.home,name='home'),
+    path('add',views.add_fun,name='add'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('delete/<int:id>',views.deletefun,name='delete'),
+    path('dummy',views.dummy,name='dummy'),
+
+]
